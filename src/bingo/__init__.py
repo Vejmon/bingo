4b5469436965d5457ceb37e4f931f07bb4153fee
Vejmon <vemundhell@gmail.com>
2026-02-09 00:21:30 +0100

    app: fix kink with missing module for composer, replaced with docx.

--- src/bingo/__init__.py
+++ src/bingo/__init__.py
@@ -2,9 +2,7 @@ from random import shuffle
 
 def get_random_facts():
     with open("../../input.txt") as input_txt:
-        facts = []
-        for i in range(1, 13):
-            facts.append( input_txt.readline().strip())
+        facts = [line.strip() for line in input_txt]
 
     shuffle(facts)
     return {f"fact{i+1}": facts[i] for i in range(len(facts))}
--- src/bingo/bingo.py
+++ src/bingo/bingo.py
@@ -1,11 +1,30 @@
-from docxtpl import DocxTemplate, Subdoc
+from docxtpl import DocxTemplate
 from uuid import uuid4
+from docx import Document
+from os import remove
 
 from __init__ import get_random_facts
 
-doc = DocxTemplate("../../3x4.docx")
-facts = get_random_facts()
+pages = []
 
-doc.render(facts)
+for i in range(1, 20):
+    page = DocxTemplate("../../3x4.docx")
+    # todo: ensure all facts are used at least once for the first 10 pages
+    page.render(get_random_facts())
+    name = f"{uuid4()}.docx"
+    page.save(f"../../tmp/{name}")
+    pages.append(name)
+
+doc = Document(f"../../tmp/{pages[0]}")
+doc.add_page_break()
+remove(f"../../tmp/{pages[0]}")
+for index, page in enumerate(pages[1:]):
+    sp = Document(f"../../tmp/{page}")
+    if index < len(pages) - 1:
+        sp.add_page_break()
+
+    for element in sp.element.body:
+        doc.element.body.append(element)
+    remove(f"../../tmp/{page}")
 
 doc.save(f"../../out/{uuid4()}.docx")
